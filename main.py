2ad9b2928b275b5d639ca1b9a68e122f246df6b4
Taft Sanders <tasander@redhat.com>
2018-10-14 22:54:57 -0400

    condensed functioning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2
 
+from __future__ import print_function
 from collections import Counter
 import time
 import subprocess
@@ -10,30 +11,32 @@ import operator
 FOREMAN_HTTP_LOG = '/var/log/httpd/foreman-ssl_access_ssl.log'
 PUPPET_HTTP_LOG = '/var/log/httpd/puppet_access_ssl.log'
 OFFENDERS_LOG = '/var/log/offenders'
-FOREMAN_COMMAND = ['cat', FOREMAN_HTTP_LOG, '|', 'cut', '-d\' \'', '-f1',  '|', 'sort', '|', 'uniq -c']
-FOREMAN_COMMAND2 = "cat " + FOREMAN_HTTP_LOG + " | cut -d' ' -f1 | sort | uniq -c"
-PUPPET_COMMAND = ['cat', PUPPET_HTTP_LOG, '|', 'cut', '-d\' \'', '-f1',  '|', 'sort', '|', 'uniq -c']
+MOTD = '/etc/motd'
 
-def foreman_header():
-    print("""
+def header(service,location):
+    if service == 'foreman':
+        header = """
 #########################
 FOREMAN CHECK-INS
 #########################
-""")
-
-def puppet_header():
-    print("""
+"""
+        print('Between ' + foreman_start_date() + ' and ' + \
+        foreman_end_date() + ':', file = open(location, 'a'))
+    elif service == 'puppet':
+        header = """
 #########################
 PUPPET CHECK-INS
 #########################
-""")
+"""
+        print('Between ' + puppet_start_date() + ' and ' + \
+        puppet_end_date() + ':', file = open(location, 'a'))
+    return header
 
 def date_header():
-    print('')
-    print('######################')
-    print(time.strftime('%Y-%m-%d %H:%M'))
-    print('######################')
-    print('')
+    header = """\n######################\n""" + \
+    time.strftime('%Y-%m-%d %H:%M') + \
+    """\n######################\n"""
+    return header
 
 def foreman_start_date():
     # open file, grab first line date/time
@@ -63,10 +66,9 @@ def puppet_end_date():
         format = last_line.split()[3]
     return format[1:]
 
-def write_foreman():
+def write_foreman(location):
     # write FOREMAN_COMMAND to file
-    foreman_header()
-    print('')
+    print(header('foreman', location), file = open(location, 'a'))
     ips = []
     with open(FOREMAN_HTTP_LOG, 'r') as file:
         log = file.readlines()
@@ -78,12 +80,11 @@ def write_foreman():
         cnt[word] += 1
     sorted_x = sorted(cnt.items(), key=operator.itemgetter(1), reverse=True)
     for value in sorted_x:
-        print(value)
+        print(value, file = open(location, 'a'))
 
-def write_puppet():
+def write_puppet(location):
     # write PUPPET_COMMAND to file
-    puppet_header()
-    print('')
+    print(header('puppet', location), file = open(location, 'a'))
     ips = []
     with open(PUPPET_HTTP_LOG, 'r') as file:
         log = file.readlines()
@@ -93,33 +94,34 @@ def write_puppet():
     cnt = Counter()
     for word in ips:
         cnt[word] += 1
-
+    sorted_x = sorted(cnt.items(), key=operator.itemgetter(1), reverse=True)
+    for value in sorted_x:
+        print(value, file = open(location, 'a'))
+        
 def create_file():
     # create log file
-    with open(OFFENDERS_LOG, 'w') as file:
+    with open(OFFENDERS_LOG, 'a') as file:
         file.close()
 
 def create_motd():
     # create motd
-    with open('/etc/motd', 'w') as file:
+    with open(MOTD, 'a') as file:
         file.close()
 
-def write_output():
-    print('Between ' + foreman_start_date() + ' and ' + foreman_end_date() + ':')
-    write_foreman()
-    print('')
-    print('Between ' + puppet_start_date() + ' and ' + puppet_end_date() + ':')
-    write_puppet()
+def write_output(location):
+    print(write_foreman(location), file = open(location, 'a'))
+    print(write_puppet(location), file = open(location, 'a'))
 
 def main():
     if not os.path.exists(OFFENDERS_LOG):
         create_file()
-    with open(OFFENDERS_LOG, 'a') as file:
-        file.write(str(date_header()))
-        file.write(str(write_output()))
-    if not os.path.exists('/etc/motd'):
-        create_motd()
-    with open('/etc/motd', 'w') as file:
-        file.write(str(write_output()))
+    with open(OFFENDERS_LOG, 'a') as log:
+        log.write(str(date_header())+ '\n')
+        log.write(str(write_output(OFFENDERS_LOG))+ '\n')
+#    if not os.path.exists('/etc/motd'):
+#        create_motd()
+#    with open(MOTD, 'w') as motd:
+#        motd.write(str(write_output(MOTD)))
 
-main()
+if __name__ == '__main__':
+    main()
